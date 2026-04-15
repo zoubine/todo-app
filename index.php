745d120e0212d75beb3adf322ff8c5b7e66733b4
YourStudentGitHubUsername <student-email@example.com>
2026-04-15 21:37:40 +0100

    fixese

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<?php
-// Force HTTPS - MUST be first
-if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
-    $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header("Location: $redirect");
-    exit();
-}
 
-// Then your regular code...
-session_start();
-require_once 'config.php';
-?>
 <?php
 require_once 'config.php';
 require_once 'auth.php';
