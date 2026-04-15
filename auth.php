31e95b2cf169c89f1f35970f14ca597570082132
ZOUBINE SOULAIMANE <s.zoubine@edu.umi.ac.ma>
2026-04-15 22:24:37 +0100

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,7 +1,26 @@
 <?php
 function isLoggedIn() {
     return isset($_SESSION['user_id']);
+}<?php
+function isLoggedIn() {
+    return isset($_SESSION['user_id']);
 }
+
+function requireLogin() {
+    if (!isLoggedIn()) {
+        header('Location: login.php');
+        exit();
+    }
+}
+
+function getUserId() {
+    return $_SESSION['user_id'] ?? null;
+}
+
+function getUsername() {
+    return $_SESSION['username'] ?? null;
+}
+?>
 
 function requireLogin() {
     if (!isLoggedIn()) {
